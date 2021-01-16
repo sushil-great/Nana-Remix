b9cc51514d9026ccd89d91db299b2f3c13392fdf
ポキ <aman_a@aol.com>
2021-01-16 16:09:59 +0500

    chat link !

--- nana/plugins/assistant/inline_mod/inline_search.py
+++ nana/plugins/assistant/inline_mod/inline_search.py
@@ -11,20 +11,27 @@ async def inline_search(string, answers):
     query = string.split(None, 1)[1]
     async for message in app.search_global(query, limit=50):
         chat = message.chat
-        keyboard = InlineKeyboard(row_width=2)
-        keyboard.add(
+        keyboard = InlineKeyboard()
+        keyboard.row(
             InlineKeyboardButton(
-                'Link',
+                'Message 🔗',
                 url=message.link,
             ),
         )
+        if chat.username:
+            keyboard.row(
+                InlineKeyboardButton(
+                    'Chat 🔗',
+                    url=f'https://t.me/{chat.username}',
+                ),
+            )
         try:
             text = message.text[:99] + '...'
         except TypeError:
             text = message.caption[:99] + '...'
         answers.append(
             InlineQueryResultArticle(
-                title=f'From: {chat.title} | {chat.id}',
+                title=f'Chat: {chat.title} | {chat.id} | {chat.type}',
                 description=text,
                 input_message_content=InputTextMessageContent(
                     f'**Chat:** `{chat.id}`\n\n' + text,
