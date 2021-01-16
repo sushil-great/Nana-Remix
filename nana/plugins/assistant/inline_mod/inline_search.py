29c9471c40a2329ff948331f52e3ea5d1b050025
ポキ <aman_a@aol.com>
2021-01-16 15:33:57 +0500

    cleanup

--- nana/plugins/assistant/inline_mod/inline_search.py
+++ nana/plugins/assistant/inline_mod/inline_search.py
@@ -10,33 +10,26 @@ from nana import app
 async def inline_search(string, answers):
     query = string.split(None, 1)[1]
     async for message in app.search_global(query, limit=50):
-        user = message.from_user
+        chat = message.chat
         keyboard = InlineKeyboard(row_width=2)
         keyboard.add(
             InlineKeyboardButton(
                 'Link',
                 url=message.link,
             ),
-            InlineKeyboardButton(
-                'User',
-                url=f'tg://user?id={user.id}',
-            ),
         )
-        thumbnail = f'https://t.me/{user.username}' if user.username else None
         try:
             text = message.text[:45] + '...'
         except TypeError:
             text = message.caption[:45] + '...'
-        chat = message.chat.id
         answers.append(
             InlineQueryResultArticle(
-                title=f'From: {user.first_name} | {user.id}',
+                title=f'From: {chat.first_name} | {chat.id}',
                 description=text,
                 input_message_content=InputTextMessageContent(
-                    f'**From:** `{user.id}`\n**Chat: **`{chat}`\n\n' + text,
+                    f'**Chat:** `{chat}`\n\n' + text,
                     parse_mode='markdown',
                 ),
                 reply_markup=keyboard,
-                thumb_url=thumbnail,
             ),
         )
